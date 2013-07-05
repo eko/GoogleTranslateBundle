0dd793466bfe6c7312016a3409f83a692396464a
Vincent Composieux <composieux@ekino.com>
2013-07-05 18:34:01 +0200

    Add base units tests

---
+++ Tests/Method/DetectorTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Eko\GoogleTranslateBundle\Tests\Method;
+
+use Eko\GoogleTranslateBundle\Translate\Method\Detector;
+
+/**
+ * Detector class test
+ *
+ * @author Vincent Composieux <composieux@ekino.com>
+ */
+class DetectorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Detector $detector Detector service
+     */
+    protected $detector;
+
+    /**
+     * Set up methods services
+     */
+    protected function setUp()
+    {
+        $this->detector = $this->getMock(
+            '\Eko\GoogleTranslateBundle\Translate\Method\Detector',
+            array('process'),
+            array('fakeapikey')
+        );
+    }
+
+    /**
+     * Test simple detect method
+     */
+    public function testSimpleDetect()
+    {
+        $this->detector
+            ->expects($this->any())
+            ->method('process')
+            ->will($this->returnValue('en'));
+
+        $language = $this->detector->detect('hi');
+        $this->assertEquals($language, 'en', 'Should return language "en"');
+    }
+}
---
+++ Tests/Method/TranslatorTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Eko\GoogleTranslateBundle\Tests\Method;
+
+use Eko\GoogleTranslateBundle\Translate\Method\Translator;
+
+/**
+ * Translator class test
+ *
+ * @author Vincent Composieux <composieux@ekino.com>
+ */
+class TranslatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Translator $translator Translator service
+     */
+    protected $translator;
+
+    /**
+     * Set up methods services
+     */
+    protected function setUp()
+    {
+        $this->translator = $this->getMock(
+            '\Eko\GoogleTranslateBundle\Translate\Method\Translator',
+            array('process'),
+            array('fakeapikey', $this->getMockDetector())
+        );
+    }
+
+    /**
+     * Test simple translate method
+     */
+    public function testSimpleTranslate()
+    {
+        $this->translator
+            ->expects($this->any())
+            ->method('process')
+            ->will($this->returnValue('salut'));
+
+        $value = $this->translator->translate('hi', 'fr', 'en');
+        $this->assertEquals($value, 'salut', 'Should return "salut"');
+    }
+
+    /**
+     * Returns mock detector
+     *
+     * @return \Fullsix\GoogleTranslateBundle\Translate\Method\Detector
+     */
+    public function getMockDetector()
+    {
+        return $this->getMockBuilder('\Eko\GoogleTranslateBundle\Translate\Method\Detector')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+}
